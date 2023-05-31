ef3777283c05714666811e2487ccde2b5fc174e8
yuri.freire <yuri_4017@hotmail.com>
2023-05-31 18:22:38 -0300

    Commite de ajuste no projeto

---
+++ src/Domain/Entity.cs
@@ -0,0 +1,15 @@
+﻿namespace IWantApp.Domain;
+
+public abstract class Entity
+{
+    public Entity()
+    {
+        Id = Guid.NewGuid(); 
+    }
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string CreatedBy { get; set; }
+    public string UpdatedBy { get; set; }
+    public DateTime CreationDate { get; set; }
+    public DateTime UpdateDate { get; set; }
+}
